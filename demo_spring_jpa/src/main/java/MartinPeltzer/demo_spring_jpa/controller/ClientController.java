8938c4ef194f35634994abad15ad7e38e45216bb
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-05 13:22:35 +0200

    ClientController eerste twee @GetMappings. Zonder service

--- demo_spring_jpa/src/main/java/MartinPeltzer/demo_spring_jpa/controller/ClientController.java
+++ demo_spring_jpa/src/main/java/MartinPeltzer/demo_spring_jpa/controller/ClientController.java
@@ -6,19 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController //had ik er nog niet achter staan. Daarom deed hij het eerst niet.
 public class ClientController {
-    //Ik heb hier nog geen service gemaakt. Dit werkt nog niet.
+
+
     @Autowired
     private ClientRepository clientRepository;
 
     @GetMapping(value = "/clients")
     public ResponseEntity<Object> getClients() {
-        List<Client> clients = clientRepository.findAll();
+        List<Client> clients = clientRepository.findAll(); //.findAll()-->Eens experimenteren met de andere methodes; zie onder
         return new ResponseEntity<>(clients, HttpStatus.OK);
     }
+    //"localhost:8080/clients"
+
+    @GetMapping(value = "/clients/{id}")
+    public ResponseEntity<Object> getClient(@PathVariable("id") long id) {
+        Client client = clientRepository.findById(id).orElse(null); //findById() nu gebruiken, hoort orElse() bij
+        return new ResponseEntity<>(client, HttpStatus.OK);
+    }
+    //"localhost:8080/clients/2"
 
 
 }
